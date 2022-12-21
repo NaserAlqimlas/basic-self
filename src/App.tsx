001564046b7188f7203c965e53f14b1d7424bc3d
NaserAlqimlas <naser.alqimlas@gmail.com>
2022-12-21 16:35:23 +0300

    added basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import JumbotronView from "./components/JumbotronView";
 
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <>
+            <div className="flex items-center justify-center flex-shrink-0 text-fuchsia-700 mr-6 font-semibold text-xl tracking-tight ">
+                Basic Self
+            </div>
+            <div className="flex justify-center">
+                <nav className="flex items-center justify-between flex-wrap bg-white p-6 sticky">
+                    <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                        <div className="text-sm lg:flex-grow">
+                            <a
+                                href="#responsive-header"
+                                className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-700 hover:text-gray-400 mr-4"
+                            >
+                                Who we are
+                            </a>
+                            <a
+                                href="#responsive-header"
+                                className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-700 hover:text-gray-400 mr-4"
+                            >
+                                What we do
+                            </a>
+                            <a
+                                href="#responsive-header"
+                                className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-700 hover:text-gray-400"
+                            >
+                                Testimonials
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <JumbotronView />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/JumbotronView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JumbotronView = () => {
+    return (
+        <div className="flex-row flex-grow">
+            <h1>Here</h1>
+            <h1>HelloHello</h1>
+        </div>
+    );
+};
+
+export default JumbotronView;
