d7aabc70169b97cb559323ef5311330bc0dd29ea
NaserAlqimlas <naser.alqimlas@gmail.com>
2022-12-22 15:44:05 +0300

    added jumbotron

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import JumbotronView from "./components/JumbotronView";
 function App() {
     return (
         <>
-            <div className="flex items-center justify-center flex-shrink-0 text-fuchsia-700 mr-6 font-semibold text-xl tracking-tight ">
+            <div className="flex items-center justify-center flex-shrink-0 text-fuchsia-700 mr-6 font-semibold text-xl ">
                 Basic Self
             </div>
-            <div className="flex justify-center">
+            <JumbotronView />
+            <div className="flex justify-center items-center">
                 <nav className="flex items-center justify-between flex-wrap bg-white p-6 sticky">
-                    <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                        <div className="text-sm lg:flex-grow">
+                    <div className="w-full block flex-shrink lg:flex lg:items-center lg:w-auto">
+                        <div className="text-m invisible md:visible">
                             <a
                                 href="#responsive-header"
                                 className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-700 hover:text-gray-400 mr-4"
@@ -32,7 +33,6 @@ function App() {
                     </div>
                 </nav>
             </div>
-            <JumbotronView />
         </>
     );
 }
--- src/components/JumbotronView.tsx
+++ src/components/JumbotronView.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import image from "../assests/placeholder.png";
 
 const JumbotronView = () => {
     return (
-        <div className="flex-row flex-grow">
-            <h1>Here</h1>
-            <h1>HelloHello</h1>
+        <div className="flex flex-row h-full">
+            <div className="flex flex-col justify-center p-10 pt-20">
+                <h1 className="text-7xl ">Lorum Ipsum</h1>
+                <h3 className="text-lg pt-2">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                </h3>
+                <button className="my-5 justify-center items-center border border-fuchsia-600 rounded-md bg-fuchsia-600 text-white hover:bg-fuchsia-700 hover:border-fuchsia-700 duration-150">
+                    Contact Us
+                </button>
+            </div>
+            <div className="pt-10 invisible lg:visible">
+                <img src={image} />
+            </div>
         </div>
     );
 };
