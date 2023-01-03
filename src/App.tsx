05cfe7a9dd2fced243650ce7e621f83890b8584f
NaserAlqimlas <naser.alqimlas@gmail.com>
2023-01-03 13:47:24 +0300

    added text and reorganized some components

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,39 @@ import WhoWeAre from "./components/WhoWeAre";
 
 function App() {
     return (
-        <>
-            <div className="flex items-center justify-center flex-shrink-0 text-fuchsia-700 mr-6 font-semibold text-xl ">
-                Basic Self
-            </div>
-            <div className="flex justify-center items-center">
-                <nav className="flex items-center justify-between flex-wrap bg-white p-6 sticky">
-                    <div className="w-full block flex-shrink lg:flex lg:items-center lg:w-auto">
-                        <div className="text-m invisible md:visible">
-                            <Link
-                                to="sec1"
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                                className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-600 hover:text-fuchsia-800 mr-4"
-                            >
-                                Who we are
-                            </Link>
+        <div>
+            <div className="h-screen">
+                <div className="flex items-center justify-center flex-shrink-0 text-fuchsia-700 mr-6 font-semibold text-xl pt-10">
+                    Basic Self
+                </div>
+                <div className="flex justify-center items-center">
+                    <nav className="flex items-center justify-between flex-wrap bg-white p-6 sticky">
+                        <div className="w-full block flex-shrink lg:flex lg:items-center lg:w-auto">
+                            <div className="text-m invisible md:visible">
+                                <Link
+                                    to="sec1"
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={500}
+                                    className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-600 hover:text-fuchsia-800 mr-4"
+                                >
+                                    Who We Are
+                                </Link>
 
-                            <a
-                                href="#responsive-header"
-                                className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-600 hover:text-fuchsia-800 mr-4"
-                            >
-                                What we do
-                            </a>
-                            <a
-                                href="#responsive-header"
-                                className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-600 hover:text-fuchsia-800"
-                            >
-                                Testimonials
-                            </a>
+                                <a className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-600 hover:text-fuchsia-800 mr-4">
+                                    What We Offer
+                                </a>
+                                <a className="block mt-4 lg:inline-block lg:mt-0 text-fuchsia-600 hover:text-fuchsia-800">
+                                    Testimonials
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                </nav>
+                    </nav>
+                </div>
+                <JumbotronView />
             </div>
-            <JumbotronView />
             <WhoWeAre id="sec1" />
-        </>
+        </div>
     );
 }
 
--- src/components/JumbotronView.tsx
+++ src/components/JumbotronView.tsx
@@ -3,16 +3,35 @@ import image from "../assests/placeholder.png";
 
 const JumbotronView = () => {
     return (
-        <div className="flex flex-row h-full">
-            <div className="flex flex-col justify-center p-10 pt-20">
-                <h1 className="text-7xl ">Lorum Ipsum</h1>
-                <h3 className="text-lg pt-2">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
-                </h3>
+        <div className="flex flex-row">
+            <div className="flex flex-col justify-center p-10">
+                <h1 className="text-7xl ">Coming Home</h1>
+                <p className="text-lg pt-2">
+                    Healing the inner child is the core of my work. I believe
+                    that as children we positively complied with everything
+                    around us, adopting patterns from our parents, school,
+                    society or anyone who came across our path and left a trail.
+                    With time, those patterns became distorted beliefs. We live
+                    by them until they became second nature, we lost touch with
+                    our essence, who we truly are.
+                </p>
+                <p className="text-lg pt-2">
+                    We became conditioned into those beliefs. Until a day comes
+                    when we can't live by them anymore, they become obstacles
+                    that hinder our growth, our wellbeing, our whole existence.
+                    This is when we start to seek professional help! The healing
+                    journey is worth every minute. In this short form of therapy
+                    you are being taken back to those early years, not to blame
+                    anyone, not to condemn anyone nor to just reminisce, but
+                    rather to start afresh, to bring you home to your real self.
+                    To move that child from a place of woundedness to a place of
+                    healing and forgiveness. To take that child from the victim
+                    status to empowering the adult self today, to achieve full
+                    potential, dreams and hopes.
+                </p>
+                <p className="text-xl pt-2">
+                    In the Basic Self all that is possible.
+                </p>
                 <button className="my-5 justify-center items-center border border-fuchsia-600 rounded-md bg-fuchsia-600 text-white hover:bg-fuchsia-700 hover:border-fuchsia-700 duration-150">
                     Contact Us
                 </button>
