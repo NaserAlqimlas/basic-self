902a239bf07fec3768a455e0705ba3947ae58c16
Nasser AlQimlas <naser.alqimlas@gmail.com>
2023-01-18 11:56:42 +0300

    added styles to whatweoffer comp

--- src/components/WhatWeOffer.tsx
+++ src/components/WhatWeOffer.tsx
@@ -10,24 +10,24 @@ const WhatWeOffer = (props: WhatWeOfferProps) => {
     return (
         <div id={id}>
             <div className="md:h-screen grid md:grid-cols-2">
-                <div className="flex bg-gradient-to-r from-fuchsia-500 to-fuchsia-800 transition  ease-in-out hover:-translate-y-1 hover:scale-110 hover:from-fuchsia-800 hover:to-fuchsia-500 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
+                <button className="flex bg-gradient-to-r from-fuchsia-600 to-fuchsia-400 hover:transition hover:ease-in-out hover:-translate-y-1 hover:scale-110 hover:from-fuchsia-700 hover:to-fuchsia-500 hover:font-bold items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
                     Hypnotherapy
-                </div>
-                <div className="flex bg-fuchsia-500 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
+                </button>
+                <button className="flex bg-gradient-to-r from-fuchsia-600 to-fuchsia-400 hover:transition hover:ease-in-out hover:-translate-y-1 hover:scale-110 hover:from-fuchsia-700 hover:to-fuchsia-500 hover:font-bold items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
                     Transpersonal Counseling & Healing
-                </div>
-                <div className="flex bg-fuchsia-500 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
+                </button>
+                <button className="flex bg-gradient-to-r from-fuchsia-600 to-fuchsia-400 hover:transition hover:ease-in-out hover:-translate-y-1 hover:scale-110 hover:from-fuchsia-700 hover:to-fuchsia-500 hover:font-bold items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
                     Personal Vision Coaching
-                </div>
-                <div className="flex bg-fuchsia-500 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
+                </button>
+                <button className="flex bg-gradient-to-r from-fuchsia-600 to-fuchsia-400 hover:transition hover:ease-in-out hover:-translate-y-1 hover:scale-110 hover:from-fuchsia-700 hover:to-fuchsia-500 hover:font-bold items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
                     Cognitive Behavior Therapy
-                </div>
-                <div className="flex bg-fuchsia-500 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
+                </button>
+                <button className="flex bg-gradient-to-r from-fuchsia-600 to-fuchsia-400 hover:transition hover:ease-in-out hover:-translate-y-1 hover:scale-110 hover:from-fuchsia-700 hover:to-fuchsia-500 hover:font-bold items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
                     The Hoffman Process Coaching
-                </div>
-                <div className="flex bg-fuchsia-500 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
+                </button>
+                <button className="flex bg-gradient-to-r from-fuchsia-600 to-fuchsia-400 hover:transition hover:ease-in-out hover:-translate-y-1 hover:scale-110 hover:from-fuchsia-700 hover:to-fuchsia-500 hover:font-bold items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
                     More...
-                </div>
+                </button>
             </div>
         </div>
     );
