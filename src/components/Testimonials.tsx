a3209c145d8babc73deb19369f50706d92374bdf
Nasser AlQimlas <naser.alqimlas@gmail.com>
2023-03-17 22:30:47 +0300

    added text to other what we offer categories

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -6,44 +6,54 @@ const Testimonials = (props: testimonialsProps) => {
     const { id } = props;
     return (
         <div id={id}>
+            <h3 className="flex items-center justify-center flex-shrink-0 font-semibold text-3xl pt-10">
+                Testimonials
+            </h3>
             <div className="grid md:grid-cols-3 m-10">
                 <div className="m-10">
                     <p>
                         Abeer offers a helping hand to take you into yourself to
                         discover your own abilities and strengths. I started
                         seeing things from a different perspective that enabled
-                        me to find peace within my self.”
+                        me to find peace within my self.
                     </p>
-                    <p>A.M. - Engineer, Bahrain</p>
+                    <br />
+                    <p className="text-fuchsia-700">A.M. - Engineer, Bahrain</p>
                 </div>
                 <div className="m-10">
                     <p>
-                        "You were in my thoughts yesterday, and I felt how much
+                        You were in my thoughts yesterday, and I felt how much
                         grateful that you thought of others enough to work on
                         making The Hoffman Process a reality in this part of the
-                        planet! Thank you for the light you brought into my
-                        life”{" "}
+                        planet! Thank you for the light you brought into my life
                     </p>
-                    <p>A.M. - Communication & Autism Therapist, Saudi Arabia</p>
+                    <br />
+                    <p className="text-fuchsia-700">
+                        A.M. - Communication & Autism Therapist, Saudi Arabia
+                    </p>
                 </div>
                 <div className="m-10">
                     <p>
-                        "My whole life was tumbling down; I had lost my
+                        My whole life was tumbling down; I had lost my
                         orientation and misplaced the reason for my existence!
                         It too.k only ONE session with Abeer and life could
-                        never have been better afterwards.”
+                        never have been better afterwards.
                     </p>
-                    <p>N.A. - Banker, Kuwait</p>
+                    <br />
+                    <p className="text-fuchsia-700">N.A. - Banker, Kuwait</p>
                 </div>
                 <div className="m-10">
                     <p>
-                        "When I first met Abeer I went in weak and I came out
+                        When I first met Abeer I went in weak and I came out
                         stronger and empowered. It is great when someone can
                         help you use your full potential as a person. Going
                         through my life later on, I felt more aware of things
-                        and people around me”
+                        and people around me
                     </p>
-                    <p>M.J. - Businesswoman, Bahrain</p>
+                    <br />
+                    <p className="text-fuchsia-700">
+                        M.J. - Businesswoman, Bahrain
+                    </p>
                 </div>
                 <div className="m-10">
                     <p>
@@ -54,13 +64,16 @@ const Testimonials = (props: testimonialsProps) => {
                         empowering: it was after a few weeks that I noticed,
                         ‘Oh, that problem has gone!” She facilitates an
                         atmosphere of self-healing and self growth, which I
-                        respect greatly.”
+                        respect greatly.
                     </p>
-                    <p> N.T. - Consultant, Bahrain</p>
+                    <br />
+                    <p className="text-fuchsia-700">
+                        N.T. - Consultant, Bahrain
+                    </p>
                 </div>
                 <div className="m-10">
                     <p>
-                        "I want to thank you for helping me to be a better and a
+                        I want to thank you for helping me to be a better and a
                         happier person. 2016 was an amazing year and not because
                         certain events that happened but because i was stronger
                         i learnt how to love me and i was more aware of my
@@ -76,9 +89,10 @@ const Testimonials = (props: testimonialsProps) => {
                         thought of sharing with you that now im ok, free,
                         cheerful and I don't think i've ever ended a year with
                         so much pride and I can say it was an amazing year
-                        Alhamdillah and thanks to you. I really appreciate you."
+                        Alhamdillah and thanks to you. I really appreciate you.
                     </p>
-                    <p> Lama - artist</p>
+                    <br />
+                    <p className="text-fuchsia-700"> Lama - artist</p>
                 </div>
             </div>
         </div>
--- src/components/WhatWeOffer.tsx
+++ src/components/WhatWeOffer.tsx
@@ -8,9 +8,34 @@ const WhatWeOffer = (props: WhatWeOfferProps) => {
     const { id } = props;
 
     const [hypnoClicked, setHypnoClicked] = useState(false);
+    const [transpersonalClicked, setTranspersonalClicked] = useState(false);
+    const [PVCClicked, setPVCClicked] = useState(false);
+    const [CBTClicked, setCBTClicked] = useState(false);
+    const [hoffmanClicked, setHoffmanClicked] = useState(false);
+    const [moreClicked, setMoreClicked] = useState(false);
 
     const toggleHypno = () => {
         setHypnoClicked(!hypnoClicked);
+    };
+
+    const toggleTrans = () => {
+        setTranspersonalClicked(!transpersonalClicked);
+    };
+
+    const togglePVC = () => {
+        setPVCClicked(!PVCClicked);
+    };
+
+    const toggleCBT = () => {
+        setCBTClicked(!CBTClicked);
+    };
+
+    const toggleHoffman = () => {
+        setHoffmanClicked(!hoffmanClicked);
+    };
+
+    const toggleMore = () => {
+        setMoreClicked(!moreClicked);
     };
 
     return (
@@ -44,21 +69,148 @@ const WhatWeOffer = (props: WhatWeOfferProps) => {
                         <span>Hypnotherapy</span>
                     )}
                 </button>
-                <div className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
-                    Transpersonal Counseling & Healing
-                </div>
-                <div className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
-                    Personal Vision Coaching
-                </div>
-                <div className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
-                    Cognitive Behavior Therapy
-                </div>
-                <div className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
-                    The Hoffman Process Coaching
-                </div>
-                <div className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white">
-                    More...
-                </div>
+                <button
+                    onClick={toggleTrans}
+                    className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white"
+                >
+                    {transpersonalClicked ? (
+                        <div>
+                            Transpersonal Counseling & Healing
+                            <br />
+                            <br />
+                            <p className="text-xl">
+                                Transpersonal Counselling & Healing is a form of
+                                counselling that studies the transpersonal,
+                                self-transcendent or spiritual aspects of the
+                                human experience. It helps connect individuals
+                                to their essence, central core, and allows full
+                                psychological and spiritual maturity
+                            </p>
+                        </div>
+                    ) : (
+                        <span>Transpersonal Counseling & Healing</span>
+                    )}
+                </button>
+                <button
+                    onClick={togglePVC}
+                    className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white"
+                >
+                    {PVCClicked ? (
+                        <div>
+                            Personal Vision Coaching
+                            <br />
+                            <br />
+                            <p className="text-xl">
+                                Personal Vision Coaching is a method that
+                                incorporates many modalities available in
+                                meeting the needs of the client, from business
+                                best practices to personal growth to even
+                                spiritual matters. This process aims to bring
+                                together and highlight all the possibilities
+                                that will help mold and shape the visioning
+                                process for individuals as they create a plan
+                                and execute the daily, weekly, monthly and
+                                yearly details.
+                            </p>
+                        </div>
+                    ) : (
+                        <span>Personal Vision Coaching</span>
+                    )}
+                </button>
+                <button
+                    onClick={toggleCBT}
+                    className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white"
+                >
+                    {CBTClicked ? (
+                        <div>
+                            Cognitive Behavior Therapy
+                            <br />
+                            <br />
+                            <p className="text-xl">
+                                Cognitive Behaviour Therapy (CBT) is a flexible
+                                approach to changing maladaptive or rigid
+                                thought processes and feelings. It addresses
+                                feelings of hopelessness, helplessness,
+                                worthlessness that accompany a present state. It
+                                aids in changing the pessimistic ideas,
+                                unrealistic expectations, perfectionism, global
+                                and overly critical self-evaluations that create
+                                disorders and sustain them. CBT helps a person
+                                recognize which life problems are critical and
+                                which are minor serving in developing positive
+                                life goals and more positive self-assessment.
+                            </p>
+                        </div>
+                    ) : (
+                        <span>Cognitive Behavior Therapy</span>
+                    )}
+                </button>
+                <button
+                    onClick={toggleHoffman}
+                    className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white"
+                >
+                    {hoffmanClicked ? (
+                        <div>
+                            The Hoffman Process Coaching
+                            <br />
+                            <br />
+                            <p className="text-xl">
+                                The Hoffman Process Coaching (for Hoffman
+                                process graduates only)* is a powerful and
+                                creative method that supports you by further
+                                integrating what you learned and experienced in
+                                the process. The purpose of coaching is to
+                                support you in applying what you learned in the
+                                process on your daily life and achieving that
+                                positive change you want for yourself. It is a
+                                supportive and personally empowering way to
+                                continue to catalyze change and transform
+                                limiting behaviours, thoughts, feelings, and
+                                attitudes.
+                            </p>
+                        </div>
+                    ) : (
+                        <span>The Hoffman Process Coaching</span>
+                    )}
+                </button>
+                <button
+                    onClick={toggleMore}
+                    className="flex bg-gradient-to-r transition-colors duration-1000 from-fuchsia-600 to-fuchsia-400 hover:transition hover:duration-500 hover:ease-in-out hover:from-fuchsia-100 hover:to-fuchsia-200 hover:text-fuchsia-700 items-center justify-center py-4 text-2xl lg:text-3xl text-white border border-white"
+                >
+                    {moreClicked ? (
+                        <div className="text-base">
+                            <span className="text-2xl">
+                                We also assist in dealing with
+                            </span>
+                            <br />
+                            <br />
+                            <ul>
+                                <li>Anexity & Stress</li>
+                                <li>Depression</li>
+                                <li>Enhanced Concentration</li>
+                                <li>Exam Stress</li>
+                                <li>Fear of public speaking</li>
+                                <li>Grief</li>
+                                <li>Inner child issues</li>
+                                <li>
+                                    Insomnia and other sleep related
+                                    difficulties
+                                </li>
+                                <li>Memory Loss</li>
+                                <li>Negative Habits</li>
+                                <li>Personal Development</li>
+                                <li>Physical Aches & pains</li>
+                                <li>Relationship issues</li>
+                                <li>Self-confidence and self-esteem issues</li>
+                                <li>Smoking cessations</li>
+                                <li>Weight concerns</li>
+                                <li>Work related stress</li>
+                            </ul>
+                        </div>
+                    ) : (
+                        <span>More...</span>
+                    )}
+                </button>
             </div>
         </div>
     );
