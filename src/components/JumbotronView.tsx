8ba21ebc873602b234ddbb5e37ffc562daf442d7
Nasser AlQimlas <naser.alqimlas@gmail.com>
2023-03-19 19:59:08 +0300

    client meeting prospective changes

--- src/components/JumbotronView.tsx
+++ src/components/JumbotronView.tsx
@@ -4,10 +4,10 @@ import { Link, animateScroll as scroll } from "react-scroll";
 
 const JumbotronView = () => {
     return (
-        <div className="flex flex-row bg-gradient-to-r from-fuchsia-600 to-fuchsia-400 text-white">
+        <div className="flex flex-row bg-gradient-to-r bg-sage text-white">
             <div className="flex flex-col justify-center p-10">
-                <h1 className="text-7xl pb-3">Coming Home</h1>
-                <p className="text-lg pt-2">
+                <h1 className="text-7xl pb-3 mx-96">Coming Home</h1>
+                <p className="text-lg pt-2 mx-96">
                     Healing the inner child is the core of my work. I believe
                     that as children we positively complied with everything
                     around us, adopting patterns from our parents, school,
@@ -16,7 +16,7 @@ const JumbotronView = () => {
                     by them until they became second nature, we lost touch with
                     our essence, who we truly are.
                 </p>
-                <p className="text-lg pt-2">
+                <p className="text-lg pt-2 mx-96">
                     We became conditioned into those beliefs. Until a day comes
                     when we can't live by them anymore, they become obstacles
                     that hinder our growth, our wellbeing, our whole existence.
@@ -30,26 +30,26 @@ const JumbotronView = () => {
                     status to empowering the adult self today, to achieve full
                     potential, dreams and hopes.
                 </p>
-                <p className="text-xl pt-2">
+                <p className="text-xl pt-2 mx-96">
                     In the Basic Self all that is possible.
                 </p>
-                <div className="pt-12 flex justify-end">
+                <div className="pt-12 flex justify-center">
                     <button>
                         <Link
                             to="contactus"
                             smooth={true}
                             offset={-70}
                             duration={500}
-                            className="w-36 p-2 my-5 flex justify-center items-center text-fuchsia-600 bg-white border rounded-md hover:font-bold duration-150"
+                            className="w-36 p-2 my-5 flex justify-center items-center text-sage bg-white border rounded-md hover:font-bold duration-150"
                         >
                             Contact Us
                         </Link>
                     </button>
                 </div>
             </div>
-            <div className="pt-10 invisible lg:visible">
+            {/* <div className="pt-10 invisible lg:visible">
                 <img src={image} alt="pic" />
-            </div>
+            </div> */}
         </div>
     );
 };
